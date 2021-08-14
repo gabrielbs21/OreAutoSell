37337a1d65f49317bbfa68cb2ba55d88d545f997
Gabriel Santos <gabrielbarretos2000@outlook.com>
2021-08-14 18:32:55 -0300

    Project started

---
+++ src/main/java/me/gabrielsantos/minecraft/oreautosell/OreAutoSell.java
@@ -0,0 +1,17 @@
+package me.gabrielsantos.minecraft.oreautosell;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class OreAutoSell extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
