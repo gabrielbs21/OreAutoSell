fe6438870df21c42dda435c3a09454549f1eab1c
Gabriel Santos <gabrielbarretos2000@outlook.com>
2021-08-14 18:55:16 -0300

    Project structured

--- src/main/java/me/gabrielsantos/minecraft/oreautosell/OreAutoSell.java
+++ src/main/java/me/gabrielsantos/minecraft/oreautosell/OreAutoSell.java
@@ -1,17 +1,28 @@
 package me.gabrielsantos.minecraft.oreautosell;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class OreAutoSell extends JavaPlugin {
 
+    public static OreAutoSell getInstance() {
+        return getPlugin(OreAutoSell.class);
+    }
+
+    @Override
+    public void onLoad() {
+        saveDefaultConfig();
+    }
+
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        try {
+            getLogger().info("Plugin enabled successfully.");
+        } catch (Throwable t) {
+            t.printStackTrace();
+            getLogger().info("An error occurred during a plugin initialization.");
+            Bukkit.getPluginManager().disablePlugin(this);
+        }
     }
 
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
-    }
 }
